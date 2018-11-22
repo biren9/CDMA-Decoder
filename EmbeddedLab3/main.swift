cd127ffdb07051548b2e6320e8888529b6d778b7
Gil Biren <gilbiren@gmail.com>
2018-11-22 08:45:39 +0100

    Initial Commit

---
+++ EmbeddedLab3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EmbeddedLab3
+//
+//  Created by Gil Biren on 22.11.18.
+//  Copyright © 2018 Gil Biren. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
